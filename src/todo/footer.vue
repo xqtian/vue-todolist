708d5115f044f45c820fe9626c1e2a75954199fb
tianxueqing <tianxq@mysteel.com>
2018-12-10 19:20:08 +0800

    message

---
+++ src/todo/footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer id="footer">
+    <span>written by me</span>
+  </footer>
+</template>
+<style lang="less" scoped>
+#footer {
+    margin-top: 40px;
+    text-align: center;
+    color: #bfbfbf;
+    font-size: 10px;
+    text-shadow: 0 1px 0 #ccc;
+  }
+</style>
+
